20e7e5cae0a671dbbcaf1c85b52f9715c34b83c7
everwishhq-commits <everwishhq@gmail.com>
2025-10-01 19:59:15 -0400

    Update splash.js

--- app/components/splash.js
+++ app/components/splash.js
@@ -15,15 +15,15 @@ export default function Splash() {
 
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-white z-50">
-      {/* Logo */}
+      {/* Logo con efecto parpadeo */}
       <img
         src="/logo.png"
         alt="Everwish Logo"
-        className="w-40 h-40 mb-6"
+        className="w-28 h-28 mb-6 animate-pulse" // más pequeño (antes w-40 h-40)
       />
 
-      {/* Loading bar */}
-      <div className="w-64 h-2 bg-pink-200 rounded-full overflow-hidden">
+      {/* Barra rosada */}
+      <div className="w-48 h-2 bg-pink-200 rounded-full overflow-hidden">
         <div className="h-full bg-pink-500 animate-progress"></div>
       </div>
     </div>
