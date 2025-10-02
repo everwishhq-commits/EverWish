942103c1d34ca6b592e10de33e2800dcc69ebb54
everwishhq-commits <everwishhq@gmail.com>
2025-10-02 01:43:17 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,14 @@
 import "./globals.css";
-import Header from "./components/header";
 
 export const metadata = {
   title: "Everwish",
-  description: "Descubre un nuevo mundo de tarjetas digitales",
+  description: "Everwish digital cards",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className="bg-white text-gray-900">
-        <Header />
-        <main className="pt-28">{children}</main>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
