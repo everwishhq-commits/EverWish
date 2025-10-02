1510cb0f1d58d42a5df3c889df269fe007d88004
everwishhq-commits <everwishhq@gmail.com>
2025-10-01 21:35:25 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,12 +1,26 @@
+"use client";
+import { useState, useEffect } from "react";
 import Splash from "./components/splash";
 
 export default function Home() {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 3000); // 3 segundos
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (showSplash) {
+    return <Splash />;
+  }
+
   return (
-    <>
-      <Splash />
-      <main className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-3xl font-bold">intento 🎉</h1>
-      </main>
-    </>
+    <main className="flex flex-col items-center justify-center h-screen">
+      {/* Aquí ya va tu página real */}
+      <h1 className="text-3xl font-bold">Bienvenido a Everwish 🎉</h1>
+    </main>
   );
 }
