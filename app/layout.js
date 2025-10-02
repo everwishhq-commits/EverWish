db8be7de3e70dce187c9aa8b738ee6bd030816fb
everwishhq-commits <everwishhq@gmail.com>
2025-10-02 00:57:24 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,14 @@
-"use client";
 import "./globals.css";
-import { useState } from "react";
-import Splash from "./components/splash";
-import Header from "./components/header";
+
+export const metadata = {
+  title: "Everwish",
+  description: "Everwish Cards App",
+};
 
 export default function RootLayout({ children }) {
-  const [splashDone, setSplashDone] = useState(false);
-
   return (
-    <html lang="es">
-      <body>
-        {!splashDone && <Splash onFinish={() => setSplashDone(true)} />}
-        {splashDone && <Header show={splashDone} />}
-        <main className="pt-40">{children}</main>
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
   );
 }
