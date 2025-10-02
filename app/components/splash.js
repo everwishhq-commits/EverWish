c65039f462edba800cae02ffe8409710ecf0c970
everwishhq-commits <everwishhq@gmail.com>
2025-10-01 21:46:15 -0400

    Update splash.js

--- app/components/splash.js
+++ app/components/splash.js
@@ -13,22 +13,17 @@ export default function Splash() {
         }
         return old + 10;
       });
-    }, 300); // 3 segundos total
-
+    }, 300);
     return () => clearInterval(interval);
   }, []);
 
   return (
     <div className="fixed inset-0 flex flex-col items-center justify-center bg-black z-50">
-      {/* LOGO */}
       <img
         src="/logo.png"
         alt="Everwish Logo"
-        className="w-48 h-auto animate-pulse" 
-        // w-48 = ~192px ancho, ajusta a w-40 o w-56 según quieras más chico o más grande
+        className="w-40 h-auto animate-pulse"
       />
-
-      {/* BARRA */}
       <div className="w-56 h-2 bg-gray-700 rounded-full mt-6 overflow-hidden">
         <div
           className="h-2 bg-pink-500 rounded-full transition-all duration-300"
