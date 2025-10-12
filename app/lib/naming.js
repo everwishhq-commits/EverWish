f59b3546107ce5e1960bb7251edfb9600451f5ba
everwishhq-commits <everwishhq@gmail.com>
2025-10-11 23:27:39 -0400

    Update naming.js

--- app/lib/naming.js
+++ app/lib/naming.js
@@ -1,1 +1,8 @@
 
+export function getRelatedName(name, suffix) {
+  if (!name) return "";
+  // Si el nombre tiene 1A, lo reemplaza por 1B o viceversa
+  if (name.endsWith("1A")) return name.replace("1A", suffix);
+  if (name.endsWith("1B")) return name.replace("1B", suffix);
+  return `${name}_${suffix}`;
+}
