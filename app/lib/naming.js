49356f1af0f0f61e57ae91087e1ae3c0ea89f728
everwishhq-commits <everwishhq@gmail.com>
2025-10-16 22:47:44 -0400

    Delete app/lib directory

--- app/lib/naming.js
+++
@@ -1,6 +0,0 @@
-export function getRelatedName(name, suffix) {
-  if (!name) return "";
-  if (name.endsWith("1A")) return name.replace("1A", suffix);
-  if (name.endsWith("1B")) return name.replace("1B", suffix);
-  return `${name}_${suffix}`;
-}
