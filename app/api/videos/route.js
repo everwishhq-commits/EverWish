f89f8f69d457d77f44fc8780243613d92b28cefc
everwishhq-commits <everwishhq@gmail.com>
2025-10-19 20:17:49 -0400

    Update route.js

--- app/api/videos/route.js
+++ app/api/videos/route.js
@@ -4,55 +4,65 @@ export async function GET() {
       title: "Bunny Easter General",
       src: "/videos/bunny_easter_general.mp4",
       slug: "bunny_easter_general",
+      categories: ["easter", "general"],
     },
     {
-      title: "Dogcat Petsandanimals Appreciationday",
+      title: "Dogcat Pets and Animals Appreciation Day",
       src: "/videos/dogcat_petsandanimals_appreciationday.mp4",
       slug: "dogcat_petsandanimals_appreciationday",
+      categories: ["pets", "animals", "appreciationday"],
     },
     {
-      title: "Eagle July4th Independenceday 1A",
+      title: "Eagle July 4th Independence Day",
       src: "/videos/eagle_July4th_independenceday_1A.mp4",
       slug: "eagle_July4th_independenceday_1A",
+      categories: ["independence", "july4th", "usa"],
     },
     {
-      title: "Ghost Halloween Love 1A",
+      title: "Ghost Halloween Love",
       src: "/videos/ghost_halloween_love_1A.mp4",
       slug: "ghost_halloween_love_1A",
+      categories: ["halloween", "love"],
     },
     {
-      title: "Hugs Anniversary Love A1",
+      title: "Hugs Anniversary Love",
       src: "/videos/hugs_anniversary_love_A1.mp4",
       slug: "hugs_anniversary_love_A1",
+      categories: ["anniversary", "love"],
     },
     {
-      title: "Mother Mother'sDay Celebration 1A",
+      title: "Mother Mother's Day Celebration",
       src: "/videos/mother_mother'sday_celebration_1A.mp4",
       slug: "mother_mother'sday_celebration_1A",
+      categories: ["mothers-day", "celebration"],
     },
     {
-      title: "Mother Mothers'Day General 1A",
+      title: "Mother Mother's Day General",
       src: "/videos/mother_mothers'day_general_1A.mp4",
       slug: "mother_mothers'day_general_1A",
+      categories: ["mothers-day", "general"],
     },
     {
-      title: "Pumpkin Halloween General 1A",
+      title: "Pumpkin Halloween General",
       src: "/videos/pumpkin_halloween_general_1A.mp4",
       slug: "pumpkin_halloween_general_1A",
+      categories: ["halloween", "general"],
     },
     {
-      title: "Zombie Halloween Birthday 1A",
+      title: "Zombie Halloween Birthday",
       src: "/videos/zombie_halloween_birthday_1A.mp4",
       slug: "zombie_halloween_birthday_1A",
+      categories: ["halloween", "birthday"],
     },
     {
-      title: "Turkey Thanksgiving General1A",
+      title: "Turkey Thanksgiving General",
       src: "/videos/turkey_thanksgiving_general1A.mp4",
       slug: "turkey_thanksgiving_general1A",
+      categories: ["thanksgiving", "general"],
     },
   ];
 
-  return new Response(JSON.stringify(videos), {
+  return new Response(JSON.stringify({ all: videos }), {
     headers: { "Content-Type": "application/json" },
   });
-                      }
+}
