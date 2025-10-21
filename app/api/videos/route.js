436bddf7f242aedc91c648bb8e43682c9565c043
everwishhq-commits <everwishhq@gmail.com>
2025-10-20 20:26:23 -0400

    Update route.js

--- app/api/videos/route.js
+++ app/api/videos/route.js
@@ -6,14 +6,14 @@ export async function GET() {
       slug: "bunny_easter_general",
     },
     {
-      title: "Dogcat Petsandanimals Appreciationday",
+      title: "Dogcat Pets and Animals Appreciation Day",
       src: "/videos/dogcat_petsandanimals_appreciationday.mp4",
       slug: "dogcat_petsandanimals_appreciationday",
     },
     {
-      title: "Eagle July4th Independenceday 1A",
-      src: "/videos/eagle_July4th_independenceday_1A.mp4",
-      slug: "eagle_July4th_independenceday_1A",
+      title: "Eagle July4th Independence Day 1A",
+      src: "/videos/eagle_july4th_independenceday_1A.mp4",
+      slug: "eagle_july4th_independenceday_1A",
     },
     {
       title: "Ghost Halloween Love 1A",
@@ -26,14 +26,14 @@ export async function GET() {
       slug: "hugs_anniversary_love_A1",
     },
     {
-      title: "Mother Mother'sDay Celebration 1A",
-      src: "/videos/mother_mother'sday_celebration_1A.mp4",
-      slug: "mother_mother'sday_celebration_1A",
+      title: "Mother Mothersday Celebration 1A",
+      src: "/videos/mother_mothersday_celebration_1A.mp4",
+      slug: "mother_mothersday_celebration_1A",
     },
     {
-      title: "Mother Mothers'Day General 1A",
-      src: "/videos/mother_mothers'day_general_1A.mp4",
-      slug: "mother_mothers'day_general_1A",
+      title: "Mother Mothersday General 1A",
+      src: "/videos/mother_mothersday_general_1A.mp4",
+      slug: "mother_mothersday_general_1A",
     },
     {
       title: "Pumpkin Halloween General 1A",
@@ -46,9 +46,9 @@ export async function GET() {
       slug: "zombie_halloween_birthday_1A",
     },
     {
-      title: "Turkey Thanksgiving General1A",
-      src: "/videos/turkey_thanksgiving_general1A.mp4",
-      slug: "turkey_thanksgiving_general1A",
+      title: "Turkey Thanksgiving General 1A",
+      src: "/videos/turkey_thanksgiving_general_1A.mp4",
+      slug: "turkey_thanksgiving_general_1A",
     },
   ];
 
