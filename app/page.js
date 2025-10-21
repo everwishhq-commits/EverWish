a1b661f677e767d7f540a2fad9c5b849fa0da4ef
everwishhq-commits <everwishhq@gmail.com>
2025-10-20 22:59:17 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,9 @@
-console.log({
-  Header: typeof Header,
-  Carousel: typeof Carousel,
-  Categories: typeof Categories,
-  Footer: typeof Footer,
-});
+"use client";
+export const dynamic = "force-dynamic";
+
+import Header from "@/components/header";
+import Carousel from "@/components/carousel";
+import Categories from "@/components/categories";
+import Footer from "@/components/footer";
+import Splash from "@/components/splash";
+import { useState } from "react";
