48e9514253b012e7102fa4cdd3a7a42c8c3f7975
everwishhq-commits <everwishhq@gmail.com>
2025-11-01 11:53:51 -0400

    Create vercel-reset.js

---
+++ vercel-reset.js
@@ -0,0 +1,4 @@
+// 🚨 Este archivo solo fuerza un redeploy completo
+// No tiene función en producción, se puede borrar luego.
+export const forceVercelRedeploy = true;
+console.log("🧹 Forzando limpieza completa de build cache en Vercel...");
