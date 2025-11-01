97ea214b6bb0328914b9d23804a0188209c603f4
everwishhq-commits <everwishhq@gmail.com>
2025-11-01 01:25:55 -0400

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,14 +2,14 @@
 export const dynamic = "force-dynamic";
 
 import { useState } from "react";
-import Header from "@/components/header";
-import Carousel from "@/components/carousel";
-import Categories from "@/components/categories";
-import Footer from "@/components/footer";
-import Splash from "@/components/splash";
+import Header from "../components/Header";
+import Carousel from "../components/Carousel";
+import Categories from "../components/Categories";
+import Footer from "../components/Footer";
+import Splash from "../components/Splash";
 
 export default function Page() {
-  const [loading, setLoading] = useState(false); // 🔥 splash desactivado temporalmente
+  const [loading, setLoading] = useState(false); // splash desactivado temporalmente
 
   return (
     <>
@@ -53,4 +53,4 @@ export default function Page() {
       )}
     </>
   );
-}
+            }
