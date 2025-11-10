69740c4b07c816616a7885cd0eb3638868e9b398
everwishhq-commits <everwishhq@gmail.com>
2025-11-10 17:05:03 -0500

    Update page.js

--- app/edit/[slug]/page.js
+++ app/edit/[slug]/page.js
@@ -51,7 +51,7 @@ export default function EditPage({ params }) {
         onContextMenu={(e) => e.preventDefault()}
         className="
           w-[500px] sm:w-[320px] md:w-[340px]
-          h-[430px]
+          h-[730px]
           aspect-[4/5]
           rounded-2xl shadow-lg
           object-cover object-center
